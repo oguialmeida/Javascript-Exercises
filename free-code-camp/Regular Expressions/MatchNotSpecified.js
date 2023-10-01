675137ac3982f42addf68c3906e6428cc17bea4e
Guilherme Gomes <gui.rock08@gmail.com>
2023-10-01 20:01:04 -0300

    Update

---
+++ free-code-camp/Regular Expressions/MatchNotSpecified.js
@@ -0,0 +1,5 @@
+/* Create a single regex that matches all characters that are not a number or a vowel. Remember to include 
+the appropriate flags in the regex. */
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou^0-9]/gi;
+let result = quoteSample.match(myRegex);
