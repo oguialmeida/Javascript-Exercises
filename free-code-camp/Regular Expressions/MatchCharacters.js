9b788430e241d058d3557e983628549b50dd3b3e
Guilherme Gomes <gui.rock08@gmail.com>
2023-10-08 11:30:42 -0300

    Updating exercises

---
+++ free-code-camp/Regular Expressions/MatchCharacters.js
@@ -0,0 +1,5 @@
+/* You want to find matches when the letter s occurs one or more times in Mississippi. Write a regex that uses 
+the + sign. */
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g;
+let result = difficultSpelling.match(myRegex);
