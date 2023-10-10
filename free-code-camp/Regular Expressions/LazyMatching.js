1fd596696905271769a6cc2835964b3fe49776eb
Guilherme Gomes <gui.rock08@gmail.com>
2023-10-09 21:15:13 -0300

    More about regex

---
+++ free-code-camp/Regular Expressions/LazyMatching.js
@@ -0,0 +1,5 @@
+/* Fix the regex /<.*>/ to return the HTML tag <h1> and not the text "<h1>Winter is coming</h1>". Remember 
+the wildcard . in a regular expression matches any character. */
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Altere esta linha
+let result = text.match(myRegex);
