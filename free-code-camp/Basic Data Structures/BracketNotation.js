49887b28d111a8f9b04829b4f450d9b66bfb0664
Guilherme Gomes <gui.rock08@gmail.com>
2023-10-23 22:21:42 -0300

    Adding new exercises about arrays

---
+++ free-code-camp/Basic Data Structures/BracketNotation.js
@@ -0,0 +1,3 @@
+// In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+let myArray = ["a", "hug", "c", "d"];
+console.log(myArray);
