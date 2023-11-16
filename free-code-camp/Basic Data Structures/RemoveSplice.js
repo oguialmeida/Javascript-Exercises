16e68a279b226ac227be1a66ec7855b7d3cb139c
Guilherme Gomes <gui.rock08@gmail.com>
2023-11-15 23:27:54 -0300

    Update

---
+++ free-code-camp/Basic Data Structures/RemoveSplice.js
@@ -0,0 +1,7 @@
+// We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+
+arr.splice(0,1);
+arr.splice(3,4);
+
+console.log(arr);
