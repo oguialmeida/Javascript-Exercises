a7eb8c81f9e278915e67892f3caac508ee0e50a1
Guilherme Gomes <gui.rock08@gmail.com>
2023-12-31 11:23:01 -0300

    Adding new exercises about objects

---
+++ free-code-camp/Object Oriented Programming/DefineConstructor.js
@@ -0,0 +1,6 @@
+//Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+function Dog() {
+    this.name = "Floquinho",
+    this.color = "white",
+    this.numLegs = 4
+}
