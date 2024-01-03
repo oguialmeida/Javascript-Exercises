c8e5096207fd01d069e18be7b329a5683ba0eb0f
Guilherme Gomes <gui.rock08@gmail.com>
2024-01-03 20:23:55 -0300

    Update

---
+++ free-code-camp/Object Oriented Programming/VerifyObjects.js
@@ -0,0 +1,9 @@
+// Create a new instance of the House constructor, calling it myHouse and passing a number of bedrooms. Then, use instanceof to verify that it is an instance of House.
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms
+}
+
+let myHouse = new House(3)
+
+myHouse instanceof House
+// Only change code below this line
